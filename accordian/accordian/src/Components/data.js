4a095c62886224443ae6831e9c5beb354fbb60f3
Anna Binoy <annabinoy18@gmail.com>
2024-10-04 08:08:10 +0530

    added file

---
+++ accordian/accordian/src/Components/data.js
@@ -0,0 +1,29 @@
+const data = [
+    {
+      id  : '1',
+      question: "How can students manage stress during exams?",
+      answer:
+        "Students can manage stress by creating a study schedule, taking regular breaks, practicing mindfulness or meditation, staying physically active, and ensuring they get enough sleep. It’s also important to reach out for support from friends, family, or counselors if stress becomes overwhelming.",
+    },
+    {
+      id  : '2',
+      question: "What are some healthy habits for students to maintain during the school year?",
+      answer:
+        "Students should focus on balanced nutrition, regular exercise, staying hydrated, and getting at least 7-8 hours of sleep each night. Time management and setting realistic goals are also key for maintaining mental well-being and avoiding burnout.",
+    },
+    {
+      id  : '3',
+      question: "How can students deal with feelings of loneliness or isolation?",
+      answer:
+        "Students can combat loneliness by joining clubs or social groups, participating in extracurricular activities, staying connected with friends and family, and seeking support from a school counselor or therapist. Building a routine that includes social interaction, even virtually, can also help",
+    },
+    {
+      id  : '4',
+      question: "What are the signs of student burnout, and how can it be prevented?",
+      answer:
+        "Signs of burnout include exhaustion, lack of motivation, irritability, and a decline in academic performance. To prevent burnout, students should balance their workload with leisure activities, prioritize self-care, and set realistic expectations. Taking breaks, practicing relaxation techniques, and reaching out for help when needed are also effective strategies.",
+    },
+  ];
+
+  export default data;
+
