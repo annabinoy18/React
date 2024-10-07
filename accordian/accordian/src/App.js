87d11f56be6e809967af20e13c517c5ee4e3e566
Anna Binoy <annabinoy18@gmail.com>
2024-10-07 22:31:03 +0530

    start rating

--- accordian/accordian/src/App.js
+++ accordian/accordian/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Accordian from './Components';
 import RandomColor from './Components/random-color';
+import StarRating from './Components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/*<Accordian/>*/}
       <RandomColor/>
+      <StarRating noOfStar={10}/>
     </div>
   );
 }
